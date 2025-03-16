20c0a804c4fe0a8874bfb8596341622b4eb95d82
aniezlouie <lodianiez@gmail.com>
2025-03-16 11:37:54 +0800

    create prisma models

---
+++ prisma/migrations/20250316033620_created_tables/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE `Project` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ServiceOrder` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `project_id` INTEGER NOT NULL,
+    `created_date` DATETIME(3) NOT NULL,
+    `updated_date` DATETIME(3) NOT NULL,
+    `is_approved` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ServiceOrder` ADD CONSTRAINT `ServiceOrder_project_id_fkey` FOREIGN KEY (`project_id`) REFERENCES `Project`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
